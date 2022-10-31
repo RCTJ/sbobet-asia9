ec36691c7dd2e6ceb6f293a34bf328b62ede5e2e
SBOAAAA <fun_pu@hotmail.com>
2022-10-31 12:04:24 +0700

    test 1.0.0

---
+++ spec/sbobet-asia9-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SbobetAsia9View from '../lib/sbobet-asia9-view';
+
+describe('SbobetAsia9View', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
